377e21d3ad8699ea8d24e24036e1eb9904ababb3
rizwanshah29 <rizwanshah610@gmail.com>
2019-02-07 19:52:20 +0500

    Work with Spatie Blade templating

---
+++ resources/views/admin/posts/deletescript.blade.php
@@ -0,0 +1,45 @@
+<script type="text/javascript">
+    function confirmdel($id){
+        swal('id');
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+
+                if (willDelete) {
+
+                    $.ajax({
+                        url:'/admin_role_delete/',
+
+                        type: 'GET',
+
+                        data: {id:$id},
+
+                        success:function(flash_message){
+                            result = $.parseJSON(flash_message);
+                            if (typeof result == 'object')
+                            {
+                                swal("Poof! Your imaginary file has been deleted!", {
+                                    icon: "success",
+                                });
+                            }
+                            location.reload();
+                        },error:function(error){
+                            console.log(error)
+                            alert(error)
+                        }
+
+                    });
+                }
+
+                else {
+                    swal("Your imaginary file is safe!");
+                }
+            });
+    }
+
+</script>
